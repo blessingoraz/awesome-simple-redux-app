99563c32f6c05283632d4672a72f17cdefe51047
Blessing Orazulume <blessing.orazulume@andela.com>
2017-08-28 21:35:17 +0100

    fix cross origin request error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import * as mock from './mockData';
+
 class App extends Component {
 
   state = {
@@ -8,19 +10,40 @@ class App extends Component {
   }
 
   getTags = () => {
-    const url = "https://api.flickr.com/services/feeds/photos_public.gne?format=json";
     const { tag } = this.state;
-    fetch(`${url}&tags=${tag}`)
-      .then((res) => res.json())
-      .then((data) => console.log(data))
+    const url = tag ?`http://api.flickr.com/services/feeds/photos_public.gne?tags=${tag}&format=json&nojsoncallback=true`: ''
+
+    fetch(url)
+      .then((res) => {
+        return res.text()
+      })
+      .then((data) => {
+        const result = JSON.parse(data);
+        this.setState({
+          tagsData: result.items
+        })
+      });
   }
 
   render() {
+    const { data } = mock,
+          { tagsData} = this.state;
+    
     return (
       <div className="App">
         <div>Add tag</div>
         <input type="text" value={this.state.tag} onChange={(e) => this.setState({tag: e.target.value})}/>
         <input type="button" value="Click me" onClick={this.getTags}/>
+
+        <div>
+          {tagsData && tagsData.map((tag, index) => {
+            return (
+              <div key={index}>
+                <p>{tag.title}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     );
   }
